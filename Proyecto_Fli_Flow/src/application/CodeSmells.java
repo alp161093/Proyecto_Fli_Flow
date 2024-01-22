a9b4616cb018f01c6db5ed77eeb90049ea6121b0
alp161093 <alvaroldp93@gmail.com>
2024-01-22 21:17:44 +0100

    se añade la clase codeSmells

--- Proyecto_Fli_Flow/src/application/CodeSmells.java
+++ Proyecto_Fli_Flow/src/application/CodeSmells.java
@@ -1,5 +1,30 @@
 package application;
 
-public class CodeSmells {
+import java.util.ArrayList;
 
+public class CodeSmells {
+	private String total;
+	private ArrayList<String> listaDatos;
+	
+	public CodeSmells(String total, ArrayList<String> datos) {
+		this.total = total;
+		this.listaDatos = datos;
+	}
+
+	public String getTotal() {
+		return total;
+	}
+
+	public void setTotal(String total) {
+		this.total = total;
+	}
+
+	public ArrayList<String> getListaDatos() {
+		return listaDatos;
+	}
+
+	public void setListaDatos(ArrayList<String> listaDatos) {
+		this.listaDatos = listaDatos;
+	}
+	
 }
